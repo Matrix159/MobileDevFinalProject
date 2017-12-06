fb377574b15c8c53ca9885757a3e42e8270e9804
Cameron Sprowls <poiklasderf@gmail.com>
2017-12-05 20:11:04 -0500

    Tap to select recycler view items has been added

--- app/src/main/java/com/matrix159/finalproject/ItemAdapter.java
+++ app/src/main/java/com/matrix159/finalproject/ItemAdapter.java
@@ -1,6 +1,9 @@
 package com.matrix159.finalproject;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +12,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.OnClick;
+
 /***
  * Created by Doomninja on 11/30/2017.
  */
@@ -16,13 +21,30 @@ import java.util.List;
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
 
     private List<String> items;
+    private SparseBooleanArray selectedItems = new SparseBooleanArray();
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView item;
+        // Background of the layout, probably won't work
+        private CardView myBackground;
 
         public ViewHolder(View v){
             super(v);
+            v.setOnClickListener(this);
             item = v.findViewById(R.id.item_name_text);
+            myBackground = v.findViewById(R.id.myBackground);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (selectedItems.get(getAdapterPosition(), false)) {
+                selectedItems.delete(getAdapterPosition());
+                myBackground.setSelected(false);
+            }
+            else {
+                selectedItems.put(getAdapterPosition(), true);
+                myBackground.setSelected(true);
+            }
         }
     }
 
@@ -44,6 +66,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
     @Override
     public void onBindViewHolder(ViewHolder holder, int position){
         holder.item.setText(items.get(position));
+        holder.myBackground.setSelected(selectedItems.get(position, false));
     }
 
     // This for some reason
@@ -56,4 +79,6 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
     }
+
+
 }
